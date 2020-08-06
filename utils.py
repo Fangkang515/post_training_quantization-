e794576abf1c48698271b6ee0039360b27904978
jahn941015 <64179182+jahn941015@users.noreply.github.com>
2020-08-06 17:24:18 +0900

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,61 @@
+import os
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision
+from torch.utils.data import DataLoader
+from torchvision import datasets, models
+import torchvision.transforms as transforms
+import torch.quantization
+from torch.quantization import get_default_qconfig, quantize_jit
+from average_meter import AverageMeter
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
+
+
+def calibrate(model, data_loader):
+    model.eval()
+    with torch.no_grad():
+        for image, target in data_loader:
+            model(image)
+
+
+def evaluate(model, criterion, data_loader, neval_batches):
+    model.eval()
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+    cnt = 0
+    with torch.no_grad():
+        for image, target in data_loader:
+            output = model(image)
+            loss = criterion(output, target)
+            cnt += 1
+            acc1, acc5 = accuracy(output, target, topk=(1, 5))
+            print('.', end = '')
+            top1.update(acc1[0], image.size(0))
+            top5.update(acc5[0], image.size(0))
+            if cnt >= neval_batches:
+                 return top1, top5
+
+    return top1, top5
+
+
+def print_size_of_model(model):
+    torch.save(model.state_dict(), "temp.p")
+    print('Size (MB):', os.path.getsize("temp.p")/1e6)
+    os.remove('temp.p')
